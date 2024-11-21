3b7cf0891be74b76fa9c9091da89b38c6aa2a1e5
Emanuele <34857243+Emanuele94@users.noreply.github.com>
2024-11-21 21:56:10 +0300

    fix: added args && exported URL best pratices

--- main.py
+++ main.py
@@ -1,24 +1,37 @@
 import os
+import argparse
 from docker_linter import DockerLinter
 
-# Configurazione
-BEST_PRACTICES_URL = "https://docs.docker.com/build/building/best-practices/"
-DOCKERFILE_PATH = "Dockerfile"
+def parse_args():
+    """Handles CLI arguments."""
+    parser = argparse.ArgumentParser(description="Dockerfile Linter")
+    parser.add_argument("dockerfile", help="Path to the Dockerfile to analyze")
+    return parser.parse_args()
 
 def main():
-    # Leggi la chiave API dall'ambiente
+
+    # Read Enviroment Variable
+    BEST_PRACTICES_URL = os.getenv("BEST_PRACTICES_URL")
+    if not BEST_PRACTICES_URL:
+        BEST_PRACTICES_URL = "https://docs.docker.com/build/building/best-practices/"
+
     API_KEY = os.getenv("OPENAI_API_KEY")
     if not API_KEY:
-        raise ValueError("OPENAI_API_KEY non è impostata come variabile di ambiente.")
+        raise ValueError("OPENAI_API_KEY not set as enviroment variable")
 
     linter = DockerLinter(api_key=API_KEY, url=BEST_PRACTICES_URL)
     
-    # Scarica le best practices
+    # Download best practices
     linter.fetch_best_practices()
     
+    # Parse Args
+    args = parse_args()
+
+    DOCKERFILE_PATH = args.dockerfile
+
     # Leggi il Dockerfile
     dockerfile_content = linter.read_dockerfile(DOCKERFILE_PATH)
-    
+        
     # Analizza il Dockerfile
     analysis = linter.analyze_dockerfile(dockerfile_content)
     
